6547d0a13b6dbb268b926690c08b130422b22b90
Jake Boone <jakeboone02@gmail.com>
2023-04-16 22:04:35 -0700

    Setup tests from src instead of dist

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,3 @@
-export { Jison } from '../dist/jison.js';
+export { Jison } from '../src/jison.js';
 export { default as RegExpLexer } from 'jison-lex';
 export { default as Lexer } from 'jison-lex';
